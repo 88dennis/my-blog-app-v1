2cc243e250bbb7a5345a6707fe8a24d45b43409e
Dennis Sarmiento <dennissarmiento8080@gmail.com>
2020-08-02 23:00:37 -0400

    up

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -7,23 +7,28 @@ import NotFoundPage from './NotFoundPage';
 function ArticlePage(props) {
     const name = props.match.params.name;
     const[articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: []});
+    console.log(name);
 
-    console.log(name)
+    // useEffect(()=>{
+    // setArticleInfo({ upvotes: 3});      
+    // }, [])
 
     useEffect(()=>{
         const fetchData = async () => {
+            //in the package.json for client side type the  "proxy": "http://localhost:8000/",
             const result = await fetch("/api/articles/" + name);
             console.log(result)
-            const body = await result.json();
+            const body = await result.json();   
             console.log(body)
             setArticleInfo(body)
         }
         fetchData();
         // setArticleInfo({upvotes: Math.ceil(Math.random()*100), comments: [].concat("hello")})
-    }, [name])
+    }, [name]);
     //THE ARRAY WATCH EVERY CHANGE. WE PASS IN THE NAME SO WHEN WE CHANGE URLS, THAT'S THE ONLY TIME WE UPDATE THE ARTILCLE INFO
     //IF THERE'S NO BRACKETS THERE WILL BE AN INFINITE LOOP
     //IF JUST AN EMPTY BRACKET, IT WILL JUST UPDATE ONCE WHEN THE PAGE LOADS.. IT WILL NOT UPDATE WHEN YOU CHANGE SOMETHING
+    //you should know when to call the useEffect everytime the variable "name" changes, this will invoke the useEffect thus calling the useEffect at the ri
 
     //everytime you use traverse into a path using the Link and the React Router, it passes a prop called match
     // console.log(props.match)
@@ -46,17 +51,15 @@ function ArticlePage(props) {
 
     console.log(articleInfo)
     return (
-        <div>
+        <>
             <h1>Artice Page</h1>
             <h2>Article about {articleInfo.name}</h2>
             <h3>Now has upvotes: {articleInfo.upvotes}</h3>
-           {articleInfo.comments[0] ? <h3>Now has comments: {articleInfo.comments[0].username}</h3> : null}
+           {articleInfo.comments[0] ? <h3>user : {articleInfo.comments[0].username}</h3> : null}
            {articleInfo.comments[0] ? <h3>Now has comments: {articleInfo.comments[0].text}</h3> : null}
 
             {/* <h3>Now has comments: {articleInfo.comments[0].text}</h3> */}
-
-
-            <h3>{article.title}</h3>
+            <h3>{article.title ? article.title : "Article"}</h3>
 
             {/* ARRAYS CAN ALSO BE DISPLAYED DIRECTLY*/}
             <p>{article.content}</p>
@@ -76,7 +79,7 @@ function ArticlePage(props) {
 
                 <h3>Other Articles:</h3>
             <ArticlesListComp articles={otherArticles}/>
-        </div>
+        </>
     )
 }
 
